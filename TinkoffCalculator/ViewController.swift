c25007d7c9fa22cb4fca767281e43f841ed91060
MrLiege <mister.4100@mail.ru>
2024-01-23 18:03:43 +0400

    task 1

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Artyom on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.titleLabel?.text else { return }
+        print(buttonText)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Та-дам!")
+    }
+}
+
